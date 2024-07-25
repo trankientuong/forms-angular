508f077bb04a9779bd1816824075d1740a6f1e78
tuongtran1106 <tuongtran1106@gmail.com>
2024-07-25 11:59:15 +0700

    Exercise: Import signup form and implement reactive form approach

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  signupForm = new FormGroup({
+    email: new FormControl('', {
+      validators: [ Validators.email, Validators.required ]
+    }),
+    password: new FormControl('', {
+      validators: [ Validators.required, Validators.minLength(6) ]
+    })
+  })
+
+  onReset() {
+    this.signupForm.reset();
+  }
+
+  onSubmit() {
+    const enteredEmail = this.signupForm.value.email;
+    const enteredPassword = this.signupForm.value.password;
+    console.log(enteredEmail, enteredPassword);
+  }
+}
